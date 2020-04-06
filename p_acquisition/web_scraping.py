3a3ce00ddc33107b785c2fc7cccbadbf22af0799
vkamg <veronica.montoro.gonzalez@gmail.com>
2020-04-06 21:56:50 +0200

    creating web scraping script

---
+++ p_acquisition/web_scraping.py
@@ -0,0 +1,20 @@
+import argparse
+
+
+
+def main(country):
+    data = create_dataset('/data/raw/veronicamg.db')
+    data_cleaned = cleaning_dataset(data)
+    data_imported = import_data(data_cleaned, 'https://www.forbes.com/ajax/list/data?year=2018&uri=billionaires&type=person', 'https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)', 'https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)_per_capita')
+    analysis(data_imported, country)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="This pipeline creates a report on the forbes list and its relationship to a country's gross domestic product")
+    parser.add_argument('-c','--country', dest='country', default= None, type=str,required=True, help='Country selected to create the report')
+    args = parser.parse_args()
+
+    if isinstance(args.country, str):
+        main(args.country)
+    else:
+        print('You have to specify the argument -c with the country for which you want the report information')
