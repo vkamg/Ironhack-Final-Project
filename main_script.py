4f1d78eb4200fd37c902c17a9fbb52b10f2882cf
vkamg <veronica.montoro.gonzalez@gmail.com>
2020-04-16 21:40:52 +0200

    pipeline finished

--- main_script.py
+++ main_script.py
@@ -1,48 +1,18 @@
 import argparse
-from selenium import webdriver
 from p_acquisition.m_acquisition import acquire
 from p_reporting.m_reporting import result
 from p_wrangling.m_wrangling import clean_data
-import time
+
 
 
 def main(path, ingred):
     data = acquire(path)
     data_cleaned = clean_data(data)
-    output = result(data_cleaned, ingred)
-    recipe_name = output["recipe_name"].iloc[0]
-    num_missing_ingred = output['num_missing_ingred'].iloc[0]
-    recipe_time = output["time_preparation(min)"].iloc[0]
-    print(f"La mejor receta que puedes hacer con los ingredientes que has introducido es: {recipe_name}")
-    print(f"Te faltan {num_missing_ingred} ingredientes")
-    print(f"El tiempo de preparación es de {recipe_time} minutos")
-    go_to_recipe = input('¿Quieres ir a la receta?')
+    result_df = result(data_cleaned, ingred)
+    result_df.to_csv(f'../data/results/recipe_df.csv', index=False)
+    output = print("Gracias por utilizar FastCooking")
 
-    def normalize(string):
-        lower = string.lower()
-        a, b = 'áéíóú', 'aeiou'
-        trans = str.maketrans(a, b)
-        result = lower.translate(trans)
-        return result
-
-    input_answer = normalize(go_to_recipe)
-
-    if input_answer == 'si':
-        # Opening the Chrome and going to the Web
-        recipe_link = output["recipe_url"].iloc[0]
-        chromedriver = "/home/veronica/Ironhack/Final Project/Chrome Driver/chromedriver"
-        driver = webdriver.Chrome(chromedriver)
-        driver.get(recipe_link)
-        # Enabling cookies
-        time.sleep(3)
-        element = driver.find_elements_by_class_name('qc-cmp-button')
-        element[1].click()
-    elif input_answer == 'no':
-        recipe_link = output["recipe_url"].iloc[0]
-        print(f"Genial! Éste es el link por si quieres consultar la receta más tarde: {recipe_link}")
-    else:
-        print("Por favor, responde sí o no")
-
+    return output
 
 
 if __name__ == '__main__':
--- p_reporting/m_reporting.py
+++ p_reporting/m_reporting.py
@@ -2,6 +2,7 @@ import pandas as pd
 import Levenshtein
 import itertools
 from fuzzywuzzy import fuzz
+import webbrowser
 import ast
 from selenium import webdriver
 import time
@@ -25,7 +26,7 @@ def fuzzy_columns(column, input_string):
         return False
 
 
-def num_missing_ingred(recipe_ingred, active_ingred):
+def num_missing(recipe_ingred, active_ingred):
     missing_ingred = recipe_ingred - active_ingred
     return missing_ingred
 
@@ -34,6 +35,18 @@ def rating(match, missing):
     result = 0.75*match-0.25*missing
     return result
 
+def normalize(string):
+    lower = string.lower()
+    a, b = 'áéíóú', 'aeiou'
+    trans = str.maketrans(a, b)
+    result = lower.translate(trans)
+    return result
+
+
+def pop_recipe(url_):
+    ROUTE = url_
+    webbrowser.open_new(ROUTE)
+
 
 
 def result(recipe_df, input_ing):
@@ -41,11 +54,32 @@ def result(recipe_df, input_ing):
     for ingred in input_ingred:
         recipe_df[ingred] = recipe_df.apply(lambda x: fuzzy_columns(x["ingredients_list"], ingred), axis=1)
     recipe_df["active_ingred"] = recipe_df.select_dtypes(include=['bool']).sum(axis=1)
-    recipe_df["num_missing_ingred"] = recipe_df.apply(lambda x: num_missing_ingred(x["num_ingredients"],
+    recipe_df["num_missing_ingred"] = recipe_df.apply(lambda x: num_missing(x["num_ingredients"],
                                                                                    x["active_ingred"]),
                                                       axis=1)
     recipe_df["recipe_rate"] = recipe_df.apply(lambda x: rating(x["active_ingred"], x["num_missing_ingred"]),
                                                axis=1)
     filter_principal = recipe_df['main_category'] == "principal"
-    output_df = recipe_df[filter_principal].sort_values(by=['recipe_rate'], ascending=False).head(3)
-    return output_df
+    output = recipe_df[filter_principal].sort_values(by=['recipe_rate', 'num_missing_ingred'],
+                                                        ascending=[False, True]).head(10)
+
+    recipe_name = output["recipe_name"].iloc[0]
+    num_missing_ingred = output['num_missing_ingred'].iloc[0]
+    recipe_time = output["time_preparation(min)"].iloc[0]
+    print(f"La mejor receta que puedes hacer con los ingredientes que has introducido es: {recipe_name}")
+    print(f"Te faltan {num_missing_ingred} ingredientes")
+    print(f"El tiempo de preparación es de {recipe_time} minutos")
+    go_to_recipe = input('¿Quieres ir a la receta?')
+
+    input_answer = normalize(go_to_recipe)
+
+    if input_answer == 'si':
+        recipe_link = output["recipe_url"].iloc[0]
+        pop_recipe(recipe_link)
+    elif input_answer == 'no':
+        recipe_link = output["recipe_url"].iloc[0]
+        print(f"Genial! Éste es el link por si quieres consultar la receta más tarde: {recipe_link}")
+    else:
+        print("Por favor, responde sí o no")
+
+    return
